3da857666026bf2d247cfc340684761237881f34
danial <lipsky@knu.ua>
2022-01-09 15:38:54 +0200

    start selenium learning

---
+++ SeleniumTasks/MessageToConsole.cs
@@ -0,0 +1,19 @@
+namespace SeleniumTasks
+{
+    class MessageToConsole
+    {
+        public static void RedMessage(string message)
+        {
+            System.Console.ForegroundColor = System.ConsoleColor.Red;
+            System.Console.WriteLine(message);
+            System.Console.ForegroundColor = System.ConsoleColor.White;
+        }
+
+        public static void GreenMessage(string message)
+        {
+            System.Console.ForegroundColor = System.ConsoleColor.Green;
+            System.Console.WriteLine(message);
+            System.Console.ForegroundColor = System.ConsoleColor.White;
+        }
+    }
+}
---
+++ SeleniumTasks/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTasks
+{
+    class Program       
+    {
+
+        public static void FindElementByName(string url, string name)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+
+            IWebElement element = driver.FindElement(By.Name(name));
+
+            if(element.Displayed)
+            {
+                MessageToConsole.GreenMessage("[FindElementByName] YES");
+            }
+            else
+            {
+                MessageToConsole.RedMessage("[FindElementByName] NO");
+            }
+
+            driver.Quit();
+        }
+
+        public static void FindElementById(string url, string id)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+
+            IWebElement element = driver.FindElement(By.Id(id));
+            if(element.Displayed)
+            {
+                MessageToConsole.GreenMessage("[FindElementById] YES");
+            }
+            else
+            {
+                MessageToConsole.RedMessage("[FindElementById] NO");
+            }
+
+            driver.Quit();
+        }
+        static void Main()
+        {
+            FindElementByName(url:"https://testing.todorvachev.com/name/", name:"myName");
+            FindElementById(url:"https://testing.todorvachev.com/id/", id:"testImage");
+        }
+    }
+}
+    
