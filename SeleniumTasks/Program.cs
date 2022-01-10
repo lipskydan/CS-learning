1c90c40c36c90d5d5315100e99953f6f60e3a444
danial <lipsky@knu.ua>
2022-01-10 22:23:33 +0200

    add logic to work with Text Input Field

--- SeleniumTasks/Program.cs
+++ SeleniumTasks/Program.cs
@@ -11,8 +11,21 @@ namespace SeleniumTasks
             // ElementSelectors.FindElementByName(url:"https://testing.todorvachev.com/name/", name:"myName");
             // ElementSelectors.FindElementById(url:"https://testing.todorvachev.com/id/", id:"testImage");
             // ElementSelectors.FindElementByClassName(url:"https://testing.todorvachev.com/class-name/", className: "testClass");
-            ElementSelectors.FindElementByCssPath(url:"https://testing.todorvachev.com/css-path/", cssPath: "#post-108 > div > figure > img");
-            ElementSelectors.FindElementByXPath(url:"https://testing.todorvachev.com/css-path/", xPath: "//*[@id=\"post-108\"]/div/figure/img");
+            // ElementSelectors.FindElementByCssPath(url:"https://testing.todorvachev.com/css-path/", cssPath: "#post-108 > div > figure > img");
+            // ElementSelectors.FindElementByXPath(url:"https://testing.todorvachev.com/css-path/", xPath: "//*[@id=\"post-108\"]/div/figure/img");
+
+            SpecialElementSelector.driver = new ChromeDriver();
+            SpecialElementSelector.driver.Navigate().GoToUrl("https://testing.todorvachev.com/text-input-field/"); 
+
+            IWebElement element1 = SpecialElementSelector.FindTextInputField(FindBy.Name, whatSearch:"username");
+
+            SpecialElementSelector.SetValueToTextInputField(textField: element1, "Hello!");
+
+            SpecialElementSelector.GetValueFromTextInputField(element1, out string text1);
+            MessageToConsole.GreenMessage(text1);
+
+            SpecialElementSelector.driver.Quit();
+
         }
     }
 }
---
+++ SeleniumTasks/SpecialElementSelectors.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTasks
+{
+    enum FindBy
+    {
+        Name,
+        ClassName,
+        Id
+    }
+    class SpecialElementSelector
+    {
+        public static IWebDriver driver;
+
+        public static IWebElement FindTextInputField(FindBy findBy, string whatSearch)
+        {
+            IWebElement textField = null;
+            try
+            {
+                if(findBy == FindBy.Name) textField = driver.FindElement(By.Name(whatSearch));
+                if(findBy == FindBy.ClassName) textField = driver.FindElement(By.ClassName(whatSearch));
+                if(findBy == FindBy.Id) textField = driver.FindElement(By.Id(whatSearch));
+
+                MessageToConsole.GreenMessage("[FindTextInputField] Element was found");
+            }
+            catch(NoSuchElementException)
+            {
+                MessageToConsole.RedMessage("[FindTextInputField] No such element was found");
+            }
+            
+            return textField;
+        }
+
+        public static void SetValueToTextInputField(IWebElement textField, in string text)
+        {
+            if(textField == null) 
+            {
+                MessageToConsole.RedMessage("[SetValueToTextInputField] Text Input Field is null");
+                return;
+            }
+
+            if(text.Length > int.Parse(textField.GetAttribute("maxlength")))
+            {
+                MessageToConsole.RedMessage("[SetValueToTextInputField] Size of input text is too big for current text field");
+                return;
+            }
+
+            textField.SendKeys(text);
+            MessageToConsole.GreenMessage("[SetValueToTextInputField] Set value to Text Input Field");
+            Thread.Sleep(3000);
+        }
+
+        public static void GetValueFromTextInputField(IWebElement textField, out string text)
+        {
+            try
+            {
+                text = textField.GetAttribute("value");
+                if(text=="")
+                {
+                    MessageToConsole.RedMessage("[GetValueFromTextInputField] Value of Text Input Field is empty");
+                }
+                else
+                {
+                    MessageToConsole.GreenMessage("[GetValueFromTextInputField] Value from Text Input Field was gotten");
+                }
+            }
+            catch(NullReferenceException)
+            {
+                text = "";
+                MessageToConsole.RedMessage("[GetValueFromTextInputField] Text Field is null");
+            }
+        }
+    }
+}
